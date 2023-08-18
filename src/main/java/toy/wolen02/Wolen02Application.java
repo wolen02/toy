89c7e7f6e11bb9194c118395bffebea4fedfc988
wolen02 <wnwltjd333@naver.com>
2023-08-18 17:15:12 +0900

    First Upload

---
+++ src/main/java/toy/wolen02/Wolen02Application.java
@@ -0,0 +1,13 @@
+package toy.wolen02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wolen02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wolen02Application.class, args);
+	}
+
+}
---
+++ src/test/java/toy/wolen02/Wolen02ApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.wolen02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wolen02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
